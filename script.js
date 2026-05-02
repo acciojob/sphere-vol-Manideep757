55022c92bacf1923b9742871ffbdb2d4b1abc9e2
Manideep757 <gollapellymanideep185@gmail.com>
2026-05-02 14:57:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 function volume_sphere() {
     //Write your code here
-  
+  let radius = document.getElementById("radius").value;
+  if(radius < 0) return;
+  let volume = (4/3) * Math.PI * Math.pow(radius , 3);
+   document.getElementById("volume").value = volume.toFixed(4);
+	return false;
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
